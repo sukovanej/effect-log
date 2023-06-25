e2be6c8ef139229ea18373805aa5007da31acbd3
Milan Suk <Milansuk@email.cz>
2023-06-25 13:12:23 +0200

    fix: logger type

--- examples/example-loggin-effect.ts
+++ examples/example-loggin-effect.ts
@@ -11,19 +11,19 @@ export const exampleEffect = pipe(
   Effect.tap(() =>
     pipe(
       Effect.logError("Something's super fishy"),
-      Effect.logAnnotate("life", "is awesome"),
-      Effect.logAnnotate("sometimes", "not that awesome"),
+      Effect.annotateLogs("life", "is awesome"),
+      Effect.annotateLogs("sometimes", "not that awesome"),
     ),
   ),
-  Effect.logAnnotate("rootCause", "milan"),
+  Effect.annotateLogs("rootCause", "milan"),
   Effect.tap(() =>
-    pipe(Effect.logFatal("Don Quijote"), Effect.logAnnotate("likes", "fp-ts")),
+    pipe(Effect.logFatal("Don Quijote"), Effect.annotateLogs("likes", "fp-ts")),
   ),
   Effect.tap(() => Effect.logTrace("Hello darkness my old friend")),
   Effect.tap(() =>
     Effect.logWarning("Lesnek is a beautiful surname, is it not?"),
   ),
-  Effect.logAnnotate("myName", "Earl"),
+  Effect.annotateLogs("myName", "Earl"),
   Effect.tap(() => Effect.logDebug("Sooo sad, not annotations for me")),
   Effect.tap(() => Effect.logTrace("Never Gonna Give You Up")),
   Effect.provideSomeLayer(Logger.minimumLogLevel(LoggerLevel.All)),
