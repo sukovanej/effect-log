24a3ab131e1199ce9dc152077cd619a8696c269a
Milan Suk <Milansuk@email.cz>
2023-07-14 01:04:02 +0200

    chore: update dependencies

--- src/json.ts
+++ src/json.ts
@@ -3,21 +3,19 @@ import { threadName } from "@effect/io/Fiber/Id";
 import * as Logger from "@effect/io/Logger";
 
 export const json = (messageField?: string) =>
-  Logger.make(
-    (fiberId, logLevel, message, _cause, _context, _spans, annotations) => {
-      const tags: Record<string, unknown> = HashMap.reduceWithIndex(
-        annotations,
-        {},
-        (acc, v, k) => ({
-          ...acc,
-          [k]: v,
-        }),
-      );
+  Logger.make(({ fiberId, logLevel, message, annotations }) => {
+    const tags: Record<string, unknown> = HashMap.reduceWithIndex(
+      annotations,
+      {},
+      (acc, v, k) => ({
+        ...acc,
+        [k]: v,
+      }),
+    );
 
-      tags["logLevel"] = logLevel.label;
-      tags[messageField ?? "message"] = message;
-      tags["fiberId"] = threadName(fiberId);
+    tags["logLevel"] = logLevel.label;
+    tags[messageField ?? "message"] = message;
+    tags["fiberId"] = threadName(fiberId);
 
-      console.log(JSON.stringify(tags));
-    },
-  );
+    console.log(JSON.stringify(tags));
+  });
