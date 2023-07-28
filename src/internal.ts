9a38996ff7e466406680f6915e146644dca09c44
Milan Suk <Milansuk@email.cz>
2023-07-28 11:59:37 +0200

    feat: make Logger accept `unknown`

---
+++ src/internal.ts
@@ -0,0 +1,8 @@
+/** @internal */
+export const serializeUnknown = (u: unknown): string => {
+  try {
+    return typeof u === "object" ? JSON.stringify(u) : String(u);
+  } catch (_) {
+    return String(u);
+  }
+};
